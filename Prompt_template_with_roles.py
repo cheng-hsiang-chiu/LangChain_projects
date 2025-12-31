9a072ce6f0dca93755ca00bbda5b7667f8cc78a2
cheng-hsiang-chiu <58955345+cheng-hsiang-chiu@users.noreply.github.com>
2025-12-31 16:41:42 -0600

    Update Prompt_template_with_roles.py

--- Prompt_template_with_roles.py
+++ Prompt_template_with_roles.py
@@ -7,12 +7,12 @@ llm = ChatOpenAI(model="gpt-4o-mini", api_key='<OPENAI_API_TOKEN>')
 
 # Create a chat prompt template
 # system denotes the model role, human refers to the user's question, ai refers to the assistant answer 
-prompt_template = ChatPromptTemplate.from_message(
+prompt_template = ChatPromptTemplate.from_messages(
     [
         ("system", "You are a geography expert that returns the colors present in a country's flag."),
         ("human", "France"),
         ("ai", "blue, white, red"),
-        ("huamn", "{country}")
+        ("human", "{country}")
     ]
 )
 
