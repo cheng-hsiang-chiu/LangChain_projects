694331606c41fb0e9233802081e4c67c04760273
cheng-hsiang-chiu <58955345+cheng-hsiang-chiu@users.noreply.github.com>
2025-12-31 16:39:38 -0600

    Create Prompt_template_with_roles.py

---
+++ Prompt_template_with_roles.py
@@ -0,0 +1,24 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+
+
+
+llm = ChatOpenAI(model="gpt-4o-mini", api_key='<OPENAI_API_TOKEN>')
+
+# Create a chat prompt template
+# system denotes the model role, human refers to the user's question, ai refers to the assistant answer 
+prompt_template = ChatPromptTemplate.from_message(
+    [
+        ("system", "You are a geography expert that returns the colors present in a country's flag."),
+        ("human", "France"),
+        ("ai", "blue, white, red"),
+        ("huamn", "{country}")
+    ]
+)
+
+# Chain the prompt template and model, and invoke the chain
+llm_chain = prompt_template | llm
+
+country = "Japan"
+response = llm_chain.invoke({"country": country})
+print(response.content)
