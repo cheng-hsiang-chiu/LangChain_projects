13927088bfae6a94a8716cf3d4d96f6591c77a2a
cheng-hsiang-chiu <58955345+cheng-hsiang-chiu@users.noreply.github.com>
2025-12-31 22:27:43 -0600

    Create LangChain_RAG_workflow.py

---
+++ LangChain_RAG_workflow.py
@@ -0,0 +1,66 @@
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_core.runnables import RunnablePassthrough
+
+
+loader = PyPDFLoader('rag_vs_fine_tuning.pdf')
+data = loader.load()
+
+# Split the document using RecursiveCharacterTextSplitter
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=300,
+    chunk_overlap=50)
+docs = splitter.split_documents(data) 
+
+# Embed the documents in a persistent Chroma vector database
+embedding_function = OpenAIEmbeddings(api_key='<OPENAI_API_TOKEN>', model='text-embedding-3-small')
+vectorstore = Chroma.from_documents(
+    docs,
+    embedding=embedding_function,
+    persist_directory=os.getcwd()
+)
+
+# Configure the vector store as a retriever
+retriever = vectorstore.as_retriever(
+    search_type="similarity",
+    search_kwargs={"k": 3}
+)
+
+
+
+# Add placeholders to the message string
+message = """
+Answer the following question using the context provided:
+
+Context:
+{context}
+
+Question:
+{question}
+
+Answer:
+"""
+
+# Create a chat prompt template from the message string
+prompt_template = ChatPromptTemplate.from_messages([("human", message)])
+
+
+llm = ChatOpenAI(
+  model="gpt-4o-mini",
+  api_key='...'
+)
+
+
+
+# Create a chain to link retriever, prompt_template, and llm
+rag_chain = ({"context": retriever, "question": RunnablePassthrough()}
+            | prompt_template
+            | llm)
+
+# Invoke the chain
+response = rag_chain.invoke("Which popular LLMs were considered in the paper?")
+print(response.content)
